7437465cf09646e05867fc8d370be428c5ab107f
kenu <kenu@okjsp.pe.kr>
2025-11-16 17:03:08 +0900

    -v --version

--- bin/devwords-cli.js
+++ bin/devwords-cli.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
 const getString = require("../index.js");
+const { version } = require("../package.json");
 
 function main() {
+  const args = process.argv.slice(2);
+  
+  if (args.includes('-v') || args.includes('--version')) {
+    console.log(version);
+    return;
+  }
+  
   console.log("🙊 " + getString());
 }
 
