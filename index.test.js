c31636dacfe0a56cc97d8c7bff7b9a29dd3fe0d2
kenu <kenu@okjsp.pe.kr>
2025-11-16 16:58:48 +0900

    test cases

--- index.test.js
+++ index.test.js
@@ -1,5 +1,21 @@
 const getString = require('./index');
 
-test('should return a string', () => {
-  expect(typeof getString()).toBe('string');
+describe('devwords', () => {
+  test('should return a string', () => {
+    expect(typeof getString()).toBe('string');
+  });
+
+  test('should return a non-empty string', () => {
+    const result = getString();
+    expect(result.length).toBeGreaterThan(0);
+  });
+
+  test('should return different strings on multiple calls (eventually)', () => {
+    const results = new Set();
+    for (let i = 0; i < 5; i++) {
+      results.add(getString());
+    }
+    // Should get at least a few different strings in 5 tries
+    expect(results.size).toBeGreaterThan(1);
+  });
 });
